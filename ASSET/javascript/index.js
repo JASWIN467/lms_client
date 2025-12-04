f3e3d2e4a0c1b3ddf35119c2eb75f46ae860a783
JASWIN467 <jaswin4627@gmail.com>
2025-12-04 15:28:20 +0530

    first commit

---
+++ ASSET/javascript/index.js
@@ -0,0 +1,101 @@
+/*
+const res=-798 || 456;
+console.log(res);
+*/
+/*
+let a = 34;
+let b = 1.23433;
+console.log(typeof (b));
+*/
+/*
+let i=0;
+for(;i<=10;i++)
+    { ++i}
+    console.log(i);
+*/
+/*
+const arr=[]
+arr[0]="jassssss"
+arr[1]=43
+console.log(arr);
+*/
+
+/*
+const arr=[100,,,,,,,,,,,,,,,,,,,,]
+const b=[...arr]
+console.log(b);
+*/
+/*
+const data={
+    name:"hii welcome",
+    age:{
+        student:20,
+        game:{
+            cricket:99
+        }
+    }
+}
+console.log(data);
+console.log(data.age);
+console.log(data.age[1]);
+*/
+
+/*
+function cli() {
+    alert("welcome to india!!!!!!!!!!!!!!!!")
+}
+
+*/
+/*
+function cli()
+{
+    const heading=document.getElementById("data")
+
+    /*heading.innerHTML="<i>Hello dudeeee.....<i>"*/
+ /*   const arr=[]
+    for(let i=0;i<10;i++)
+        arr[i]=i;
+    heading.innerText=arr;  
+}
+    */
+/*
+function cli()
+{
+    const acti=document.activeElement.tagName;
+    const heading=document.getElementById("data"
+    );
+   
+    heading.innerText=acti;
+    
+    let value = document.getElementById("num").value;
+    document.getElementById("text").innerText = value;
+}
+    */
+/*
+let a=0;
+function cli()
+{
+    const heading=document.getElementById("data");
+    heading.innerText=++a;
+}
+    */
+
+const col = function () {
+    const div = document.getElementById
+        ("cont")
+    div.style.backgroundColor = "blue";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ TODO.md
@@ -0,0 +1,2 @@
+- [x] Update ASSET/javascript/sc.html to include calculator UI: display input and buttons for numbers, operations, equals, and clear.
+- [x] Correct script src from "javascript01.js" to "index.js" in sc.html.
